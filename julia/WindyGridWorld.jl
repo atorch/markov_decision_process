7cae6c70cc1e9897a12e6e76dc0e794ba70d7483
chipfranzen <chip.franzen@gmail.com>
2019-03-10 12:58:12 -0700

    Debugging, plotting, formatting, more tests.

--- julia/WindyGridWorld.jl
+++ julia/WindyGridWorld.jl
@@ -1,6 +1,18 @@
 module WindyGrid
-const actions = [0 0; -1 0; 1 0; 0 -1; 0 1]
-const windactions = [1, 0, -1]
+
+using Plots
+
+const actions = Dict(
+    [
+        "stay" => [0, 0],
+        "left" => [-1, 0],
+        "right" => [1, 0],
+        "down" => [0, 1],
+        "up" => [0, -1]
+    ]
+)
+
+const windactions = [1, -1, 0]
 
 const defaultreward = -1
 const obstaclereward = -10
@@ -14,14 +26,18 @@ const prpushdown = .2
 const discount = .9
 
 
+Obstacles = Union{Matrix, Nothing}
+
+
 struct WindyGridWorld
-    shape::Array{Int, 1}
-    target::Array{Int, 1}
-    obstacles::Array{Int, 2}
+    shape::Vector{Int}
+    target::Vector{Int}
+    obstacles::Obstacles
     discount::Float64
     prpushup::Float64
     prpushdown::Float64
     prstay::Float64
+
 
     function WindyGridWorld(shape, target, obstacles, discount, prpushup, prpushdown)
         validdiscount(discount) || error("Invalid discount $discount")
@@ -29,142 +45,11 @@ struct WindyGridWorld
         prstay = 1. - prpushup - prpushdown
         all(map(validprobability, [prpushdown, prpushup, prstay])) || error("Invalid wind probability.")
 
-        validlocation(target) || error("Target $target out of grid.")
-        validobstacles(obstacles) || error("Obstacles out of grid.")
+        validlocation(shape, target) || error("Target $target out of grid.")
+        validobstacles(shape, obstacles) || error("Obstacles out of grid.")
 
         new(shape, target, obstacles, discount, prpushup, prpushdown, prstay)
     end
-end
-
-
-function istarget(world::WindyGridWorld, location::Array{Int, 1})
-    world.target == location
-end
-
-
-function isobstacle(world::WindyGridWorld, location::Array{Int, 1})
-    n_obstacles = size(world.obstacles)[2]
-    any([ location == world.obstacles[:, i] for i=1:n_obstacles ])
-end
-
-function getreward(world::WindyGridWorld, location::Array{Int, 1})
-    if istarget(world, location)
-        targetreward
-
-    elseif isobstacle(world, location)
-        obstaclereward
-
-    else
-        defaultreward
-    end
-end
-
-function getnextlocation(
-        world::WindyGridWorld,
-        location::Array{Int, 1},
-        action::Array{Int, 1}
-    )
-    nextlocation = location + action
-    min.(max.(nextlocation, ones(Int, size(location))), world.shape)
-end
-
-function getcontinuationvalue(
-        valuefn,
-        world::WindyGridWorld,
-        location::Array{Int, 1},
-        action::Array{Int, 1},
-        windy::Int,
-        p::Float64
-    )
-    action = action + [0, windy]
-    nextlocation = getnextlocation(world, location, action)
-    p * valuefn[nextlocation...]
-end
-
-
-function getvalue(
-    valuefn,
-    world::WindyGridWorld,
-    location::Array{Int, 1},
-    action::Array{Int, 1}
-)
-    if istarget(world, location)
-        0.0
-    end
-
-    windprobs = [world.prpushup, world.prpushdown, world.prstay]
-
-    continuationvalue = sum(
-        [
-            getcontinuationvalue(
-                valuefn,
-                world,
-                location,
-                action,
-                windaction,
-                p
-            )
-            for (windaction, p) in zip(windactions, windprobs)
-        ]
-    )
-
-    reward = getreward(world, location)
-
-    reward + world.discount * continuationvalue
-end
-
-function updatevaluefn(valuefn, policyfn, world::WindyGridWorld)
-    updatedvaluefn = copy(valuefn)
-
-    for i = 1:world.shape[1], j=1:world.shape[2]
-        action = policyfn[i, j, :]
-        updatedgrid[i, j] = getvalue(valuefn, world, [i, j], action)
-    end
-    updatedvaluefn
-end
-
-function solvevaluefn!(valuefn, policyfn, world::WindyGridWorld, maxiterations::Int=50)
-    for _ = 1:maxiterations
-        valuefn = updatevaluefn(world)
-    end
-    valuefn
-end
-
-
-function updatepolicyfn(valuefn, policyfn, world::WindyGridWorld)
-    updatedgrid = copy(policyfn)
-
-    for i = 1:world.shape[1], j=world.shape[2]
-        candidates = [ getvalue(valuefn, world, [i, j], actions[i, :]) for i = 1:size(actions)[1] ]
-
-        updatedgrid[i, j] = actions[argmax(candidates), :]
-    end
-
-    updatedgrid
-end
-
-function policyiteration(maxiterations::Int=50, verbose::Bool=true)
-    world = WindyGridWorld(shape, target, obstacles, discount, prpushup, prpushdown)
-
-    valuefn = zeros(Int, world.shape)
-    policyfn = zeros(Int, world.shape..., 2)
-
-    for i in 1:maxiterations
-        valuefn = solvevaluefn!(valuefn, policyfn, world)
-
-        if verbose
-            display(round.(valuefn, digits=3))
-        end
-
-        updatedpolicy = updatepolicyfn(world)
-        if all(updated_policy .= policyfn)
-            println("Converged after $i iterations.")
-            break
-        end
-
-        policyfn = updatedpolicy
-    end
-    world
 end
 
 
@@ -177,17 +62,203 @@ function validdiscount(d)
     0 < d <= 1
 end
 
-function validlocation(gridshape::Array{Int, 1}, location::Array{Int, 1})
+
+function validlocation(gridshape::Vector{Int}, location::Vector{Int})
     all(location .>= 1) && all(location .<= gridshape)
 end
 
-function validlocation(gridshape::Array{Int, 1}, location::Array{Int, 2})
+
+function validlocation(gridshape::Vector{Int}, location::Matrix{Int})
     all(location .>= 1) && all(location .<= gridshape)
 end
 
 
-function validobstacles(gridshape::Array{Int, 1}, obstacles::Array{Int, 2})
-    all(mapslices(obstacle -> validlocation(gridshape, obstacle), obstacles, dims=[2]))
+function validobstacles(gridshape::Vector{Int}, obstacles::Obstacles)
+    obstacles == nothing || all(mapslices(obs -> validlocation(gridshape, obs), obstacles, dims=[2]))
+end
+
+
+function istarget(world::WindyGridWorld, location::Vector{Int})
+    world.target == location
+end
+
+
+function isobstacle(world::WindyGridWorld, location::Vector{Int})
+    if world.obstacles == nothing
+        false
+    else
+        any(mapslices(obs -> location == obs, world.obstacles, dims = [2]))
+    end
+end
+
+
+function getreward(world::WindyGridWorld, location::Vector{Int})
+    if istarget(world, location)
+        targetreward
+
+    elseif isobstacle(world, location)
+        obstaclereward
+
+    else
+        defaultreward
+    end
+end
+
+
+function getnextlocation(
+        world::WindyGridWorld,
+        location::Vector{Int},
+        action::Vector{Int}
+)
+    nextlocation = location + action
+    min.(max.(nextlocation, ones(Int, size(location))), world.shape)
+end
+
+
+function getcontinuationvalue(
+        valuefn,
+        world::WindyGridWorld,
+        location::Vector{Int},
+        action::Vector{Int},
+        wind_y::Int,
+        p::Float64
+)
+    action = action .+ [0, wind_y]
+
+    nextlocation = getnextlocation(world, location, action)
+    p * valuefn[nextlocation...]
+end
+
+
+function getvalue(
+    valuefn,
+    world::WindyGridWorld,
+    location::Vector{Int},
+    action::String
+)
+    if istarget(world, location)
+        targetreward
+    end
+
+    windprobs = [world.prpushup, world.prpushdown, world.prstay]
+
+    continuationvalue = sum(
+        [
+            getcontinuationvalue(
+                valuefn,
+                world,
+                location,
+                actions[action],
+                windaction,
+                p
+            )
+            for (windaction, p) in zip(windactions, windprobs)
+        ]
+    )
+
+
+    reward = getreward(world, location)
+
+    reward + world.discount * continuationvalue
+end
+
+
+function updatevaluefn(valuefn, policyfn, world::WindyGridWorld)
+    updatedvaluefn = copy(valuefn)
+
+    for i = 1:world.shape[1], j=1:world.shape[2]
+        action = policyfn[i, j]
+        updatedvaluefn[i, j] = getvalue(valuefn, world, [i, j], action)
+    end
+
+    updatedvaluefn
+end
+
+
+function solvevaluefn!(
+    valuefn::Matrix,
+    policyfn::Matrix,
+    world::WindyGridWorld,
+    maxiterations::Int=50
+)
+    for _ = 1:maxiterations
+        valuefn = updatevaluefn(valuefn, policyfn, world)
+    end
+
+    valuefn
+end
+
+
+function updatepolicyfn(valuefn, policyfn, world::WindyGridWorld)
+    updatedpolicyfn = copy(policyfn)
+
+    for i = 1:world.shape[1], j=1:world.shape[2]
+        candidates = Dict(
+            [
+                (k, getvalue(valuefn, world, [i, j], k))
+                for k in keys(actions)
+            ]
+        )
+
+        updatedpolicyfn[i, j] = argmax(candidates)
+    end
+
+    updatedpolicyfn
+end
+
+
+function policyiteration(maxiterations::Int=50, verbose::Bool=true)
+    world = WindyGridWorld(
+        shape,
+        target,
+        obstacles,
+        discount,
+        prpushup,
+        prpushdown
+    )
+
+    valuefn = zeros(Float64, world.shape...)
+    policyfn = fill("stay", world.shape...)
+
+    for i in 1:maxiterations
+        valuefn = solvevaluefn!(valuefn, policyfn, world)
+
+        if verbose
+            display(round.(valuefn, digits=3))
+        end
+
+        updatedpolicy = updatepolicyfn(valuefn, policyfn, world)
+        if all(updatedpolicy .== policyfn)
+            println("Converged after $i iterations.")
+            break
+        end
+
+        policyfn = updatedpolicy
+    end
+
+    valuefn, policyfn
+end
+
+
+function plotfns(valuefn, policyfn)
+    scale = .5
+    heatmap(transpose(valuefn))
+    xmax, ymax = size(valuefn)
+    xs = repeat(1:xmax, inner=ymax)
+    ys = repeat(1:ymax, xmax)
+    actionvec = [policyfn[xs[i], ys[i]] for i in 1:length(xs)]
+    us = [scale * actions[a][1] for a in actionvec]
+    vs = [scale * actions[a][2] for a in actionvec]
+    quiver!(xs, ys, quiver=(us, vs), color=:blue)
+    savefig("policyiteration.png")
 end
 
 end
+
+
+function main()
+    valuefn, policyfn = WindyGrid.policyiteration()
+    WindyGrid.plotfns(valuefn, policyfn)
+end
+
+main()
--- julia/tests.jl
+++ julia/tests.jl
@@ -3,6 +3,10 @@ import Main.WindyGrid
 
 using Test
 
+const target = [1, 1]
+const obstacles = [1 2; 4 3]
+const world = WindyGrid.WindyGridWorld([4, 4], target, obstacles, .9, .1, .2)
+
 @testset "validprobability" begin
     @test WindyGrid.validprobability(.5)
     @test WindyGrid.validprobability(1)
@@ -39,4 +43,55 @@ end
     @test WindyGrid.validobstacles(gridshape, [1 1])
     @test WindyGrid.validobstacles(gridshape, [1 1; 2 2])
     @test !WindyGrid.validobstacles(gridshape, [1 1; 2 -2])
+end
+
+@testset "istarget" begin
+    @test WindyGrid.istarget(world, [1, 1])
+    @test !WindyGrid.istarget(world, [1, 4])
+end
+
+@testset "isobstacle" begin
+    @test WindyGrid.isobstacle(world, [1, 2])
+    @test !WindyGrid.isobstacle(world, [1, 1])
+end
+
+@testset "getreward" begin
+    @test WindyGrid.getreward(world, target) == WindyGrid.targetreward
+    @test WindyGrid.getreward(world, obstacles[1, :]) == WindyGrid.obstaclereward
+    @test WindyGrid.getreward(world, [2, 2]) == WindyGrid.defaultreward
+end
+
+@testset "getnextlocation" begin
+    @test WindyGrid.getnextlocation(world, [1, 1], [0, 1]) == [1, 2]
+    @test WindyGrid.getnextlocation(world, [1, 1], [1, 0]) == [2, 1]
+    @test WindyGrid.getnextlocation(world, [1, 1], [-1, 0]) == [1, 1]
+    @test WindyGrid.getnextlocation(world, [4, 4], [-1, 0]) == [3, 4]
+    @test WindyGrid.getnextlocation(world, [4, 4], [1, 0]) == [4, 4]
+    @test WindyGrid.getnextlocation(world, [1, 1], [0, -1]) == [1, 1]
+end
+
+@testset "getcontinuationvalue" begin
+    valuefn = zeros(Float64, world.shape...)
+    valuefn[1, 1] = 1.
+    location = [1, 2]
+    action = [0, -1]
+    p = .5
+    wind_y = 0
+    actual = WindyGrid.getcontinuationvalue(valuefn, world, location, action, wind_y, p)
+    expected = .5
+    @test actual == expected
+    location = [1, 1]
+    action = [0, -1]
+    p = .5
+    wind_y = 1
+    actual = WindyGrid.getcontinuationvalue(valuefn, world, location, action, wind_y, p)
+    expected = .5
+    @test actual == expected
+    location = [1, 1]
+    action = [0, -1]
+    p = .5
+    wind_y = 1
+    actual = WindyGrid.getcontinuationvalue(valuefn, world, location, action, wind_y, p)
+    expected = .5
+    @test actual == expected
 end
