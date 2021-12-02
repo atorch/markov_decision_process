4088bad4756d1b3938798785beae77b04b488408
Adrian Torchiana <adriantorchiana@granular.ag>
2021-12-01 18:23:33 -0800

    New plot (action as fn of state) for cake eating problem

--- python/cake_eating.py
+++ python/cake_eating.py
@@ -13,43 +13,43 @@ VALUE_FUNCTION_CONSTANT_TERM = (
 
 PLOT_DIR = "plots"
 
-# Note: these grids are used for naive searches over the action space
-#  Where action = fraction_consumed * wealth
+# These grids are used for naive searches over the action space
+#  action = fraction_consumed * wealth
 FRACTION_CONSUMED_GRID_COARSE = np.linspace(0.01, 0.99, 4)
 FRACTION_CONSUMED_GRID_FINE = np.linspace(0.01, 0.99, 8)
 
 
 def reward_function(action):
 
-    # Note: the agent's action is the amount they consume in the current period
-    #  If they consume nothing, they receive a reward of negative infinity (they die!)
-    #  If they consume everything, they receive a large reward in the current period, but they die tomorrow
-    #  The optimal action must therefore be to consume something (but not everything)
+    # The agent's action is the amount they consume in the current period
+    # If they consume nothing, they receive a reward of negative infinity (they die!)
+    # If they consume everything, they receive a large reward in the current period, but they die tomorrow
+    # The optimal action must therefore be to consume something today (while also saving for tomorrow)
     return np.log(action)
 
 
 def optimal_policy(state):
 
-    # Note: the agent's state is their wealth (their "amount of cake")
-    #  They need to decide how much to consume today and how much to leave for future periods
-    #  The agent lives forever and is unemployed: their wealth can never increase; it can only
-    #  decrease depending on how much they consume. The optimal policy can be solved with
-    #  pen and paper. A more patient agent (with a higher discount factor) consumes less today
-    #  and saves more for future periods
+    # The agent's state variable is their wealth (their "amount of cake")
+    # They need to decide how much to consume today and how much to leave for future periods
+    # The agent lives forever and is unemployed: their wealth can never increase; it can only
+    # decrease depending on how much they consume. The optimal policy can be solved with
+    # pen and paper. A more patient agent (with a higher discount factor) consumes less today
+    # and saves more for future periods
     return (1 - DISCOUNT_FACTOR) * state
 
 
 def optimal_value_function(state):
 
-    # Note: this is the value of pursuing the optimal policy
+    # This is the value of pursuing the optimal policy, and it can be solved exactly with pen and paper
     return (1 / (1 - DISCOUNT_FACTOR)) * np.log(state) + VALUE_FUNCTION_CONSTANT_TERM
 
 
 def get_next_state(state, action):
 
-    # Note: the agent's action is how much to consume today.
-    #  Whatever is not consumed today is available tomorrow.
-    #  Wealth is a stock, consumption is a flow.
+    # The agent's action is how much to consume today.
+    # Whatever is not consumed today is available tomorrow.
+    # Wealth is a stock, consumption is a flow.
     return state - action
 
 
@@ -57,7 +57,7 @@ def optimal_policy_grid_search(
     state, approximate_value_function, fraction_consumed_grid
 ):
 
-    # Note: the grid is in [0, 1], and the action is equal to wealth * fraction consumed
+    # The fraction consumed is in [0, 1], and the action is equal to wealth * fraction_consumed
     state_mesh, fraction_consumed_mesh = np.meshgrid(state, fraction_consumed_grid)
 
     actions = state_mesh * fraction_consumed_mesh
@@ -81,13 +81,13 @@ def optimal_policy_given_approximate_value_function(state, approximate_value_fun
 
     log_wealth_coefficient = approximate_value_function.coef_[0]
 
-    # Note: on the first iteration, the coefficient on log wealth is zero (future wealth has no value),
-    #  so, without this shortcut, the agent would consume everything immediately and get a -Inf continuation value
+    # On the first iteration, the coefficient on log wealth is zero (future wealth has no value),
+    # so, without this shortcut, the agent would consume everything immediately and get a -Inf continuation value
     if log_wealth_coefficient <= 0.0:
         return state * 0.99
 
-    # Note: to arrive at this policy, write down the Bellman equation using the
-    #  approximate value function as the continuation value, and optimize with respect to the action
+    # To arrive at this policy, write down the Bellman equation using the
+    # approximate value function as the continuation value, and optimize with respect to the action
     return state / (DISCOUNT_FACTOR * log_wealth_coefficient + 1)
 
 
@@ -100,12 +100,12 @@ def get_estimated_values(
     rewards = reward_function(actions)
     next_states = get_next_state(states, actions)
 
-    # Note: the approximated value function takes log(state) as input and returns an estimated value
+    # The approximated value function takes log(state) as input and returns an estimated value
     log_next_states = np.log(next_states.reshape(-1, 1))
 
     continuation_values = approximate_value_function.predict(log_next_states)
 
-    # Note: this is the Bellman equation
+    # This is the Bellman equation
     return rewards + DISCOUNT_FACTOR * continuation_values
 
 
@@ -123,7 +123,9 @@ def calculate_approximate_solution(
     approximate_value_function = LinearRegression()
     approximate_value_function.fit(X=X, y=y)
 
-    print(f"running solver using {get_optimal_policy} to find actions given estimated value function")
+    print(
+        f"running solver using {get_optimal_policy} to find actions given estimated value function"
+    )
 
     for i in range(max_iterations):
 
@@ -176,25 +178,31 @@ def save_value_function_plot(
         log_wealth
     )
 
-    plt.plot(wealth, correct_value, label="true value function (analytical solution)")
+    plt.plot(
+        wealth,
+        correct_value,
+        label="true value function (analytical solution)",
+        linewidth=2,
+    )
 
     # Note: don't show the left and right ends of the estimated value functions
     #  so that they don't entirely cover/hide the true value function on the plot
+    idx_start, idx_stop = (1, -10)
     plt.plot(
-        wealth[1:-3],
-        approximate_value[1:-3],
+        wealth[idx_start:idx_stop],
+        approximate_value[idx_start:idx_stop],
         "--",
         label="estimated value function (using log-linear regression & first order condition for action)",
     )
     plt.plot(
-        wealth[1:-3],
-        approximate_value_coarse_grid[1:-3],
+        wealth[idx_start:idx_stop],
+        approximate_value_coarse_grid[idx_start:idx_stop],
         ":",
         label="estimated value function (using log-linear regression & coarse grid search for action)",
     )
     plt.plot(
-        wealth[1:-3],
-        approximate_value_fine_grid[1:-3],
+        wealth[idx_start:idx_stop],
+        approximate_value_fine_grid[idx_start:idx_stop],
         ":",
         label="estimated value function (using log-linear regression & fine grid search for action)",
     )
@@ -205,6 +213,34 @@ def save_value_function_plot(
 
     outdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), PLOT_DIR)
     outfile = "cake_eating_problem_value_function.png"
+    plt.savefig(os.path.join(outdir, outfile))
+    plt.close()
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    plt.plot(
+        wealth, optimal_policy(wealth), label="optimal action (analytical solution)"
+    )
+    plt.plot(
+        wealth,
+        optimal_policy_grid_search(
+            wealth,
+            approximate_value_function_coarse_grid,
+            FRACTION_CONSUMED_GRID_COARSE,
+        ),
+        label="action (using log-linear regression & coarse grid search for action)",
+    )
+    plt.plot(
+        wealth,
+        optimal_policy_grid_search(
+            wealth, approximate_value_function_fine_grid, FRACTION_CONSUMED_GRID_FINE
+        ),
+        label="action (using log-linear regression & fine grid search for action)",
+    )
+    plt.xlabel("wealth (state variable)")
+    plt.ylabel("amount consumed (action)")
+    ax.legend()
+
+    outfile = "cake_eating_problem_action.png"
     plt.savefig(os.path.join(outdir, outfile))
 
 
